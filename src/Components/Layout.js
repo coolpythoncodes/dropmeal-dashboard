ddfcb5860788494c7c545e526d2537d520e0456a
coolpythoncodes <coolpythoncodes@gmail.com>
2021-01-15 14:25:17 +0100

    initial setup

---
+++ src/Components/Layout.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Layout = () => {
+    return (
+        <div className='layout'>
+            {/* SideBar */}
+            {/* main */}
+                {/* header */}
+                {/* body */}
+        </div>
+    );
+}
+
+export default Layout;
