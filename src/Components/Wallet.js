eddde2172be14e7cc9774ff6754e2c6b214d28f2
coolpythoncodes <coolpythoncodes@gmail.com>
2021-01-25 18:17:05 +0100

    Wallet section done

--- src/Components/Wallet.js
+++ src/Components/Wallet.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import '../Sass/Wallet.scss'
 
 const Wallet = () => {
     return (
         <div className='wallet'>
-            wallet
+            <h1>Wallet</h1>
+            <div className="wallet__tableHead">
+                <div>
+                    <h2>Date</h2>
+                </div>
+                <div>
+                    <h2>User</h2>
+                </div>
+                <div>
+                    <h2>Amount Funded</h2>
+                </div>
+                <div>
+                    <h2>Wallet Balance</h2>
+                </div>
+            </div>
+            <div className="wallet__tableItem">
+                <div>
+                    <p>4 hrs ago</p>
+                </div>
+                <div>
+                    <p>delino.ndu@gmail.co</p>
+                </div>
+                <div>
+                    <p>N 5000</p>
+                </div>
+                <div>
+                    <p>N 40,000</p>
+                </div>
+            </div>
         </div>
     );
 }
