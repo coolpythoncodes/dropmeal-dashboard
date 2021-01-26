1e4787822e8c1118a3cf5dbdf23a99038517ae0f
Blissfulair <blessing.airehenbuwa@gmail.com>
2021-01-26 06:02:17 +0100

    fixed header

--- src/Components/SidebarLinks.js
+++ src/Components/SidebarLinks.js
@@ -1,14 +1,14 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import '../Sass/SidebarLinks.scss'
 
 
 const SidebarLinks = ({ title , image, href}) => {
     return (
         <li>
-             <Link to={href}>
+             <NavLink activeClassName='is-active' to={href}>
                 <img src={image} alt=""/>
                 <p>{ title }</p>
-            </Link>
+            </NavLink>
         </li>       
     );
 }
